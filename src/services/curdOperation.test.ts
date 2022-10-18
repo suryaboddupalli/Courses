e264294cda353523f65a922e2da313776c4a553f
suryaboddupalli <surya.ravi@aspiresys.com>
2022-10-18 11:14:30 +0530

    services Unit testing

--- src/services/curdOperation.test.ts
+++ src/services/curdOperation.test.ts
@@ -8,12 +8,39 @@ const payload = {
     email: 'test@gmail.com',
 }
 
+const mockCustomers = [payload]
+const id = 1
+
 describe('services', () => {
-    test('addUser', async () => {
+    test('addCustomer', async () => {
         const spy = jest.spyOn(curdOperation, 'addCustomer')
-        spy.mockReturnValueOnce(Promise.resolve('user added'))
+        spy.mockReturnValueOnce(Promise.resolve('Customer added'))
         expect(await curdOperationService.addCustomer(payload)).toBe(
-            'user added',
+            'Customer added',
+        )
+    })
+    test('getCustomers', async () => {
+        const spy = jest.spyOn(curdOperation, 'getCustomers')
+        spy.mockReturnValueOnce(Promise.resolve(mockCustomers))
+        expect(await curdOperationService.getCustomers()).toBe(mockCustomers)
+    })
+    test('getCustomer', async () => {
+        const spy = jest.spyOn(curdOperation, 'getCustomer')
+        spy.mockReturnValueOnce(Promise.resolve(mockCustomers))
+        expect(await curdOperationService.getCustomer(id)).toBe(mockCustomers)
+    })
+    test('UpdateUser', async () => {
+        const spy = jest.spyOn(curdOperation, 'updateCustomer')
+        spy.mockReturnValueOnce(Promise.resolve('Customer updated'))
+        expect(await curdOperationService.updateCustomer(payload)).toEqual(
+            'Customer updated',
+        )
+    })
+    test('Delete Customer', async () => {
+        const spy = jest.spyOn(curdOperation, 'deleteCustomer')
+        spy.mockReturnValueOnce(Promise.resolve('customer deleted'))
+        expect(await curdOperationService.deleteCustomer(id)).toEqual(
+            'customer deleted',
         )
     })
 })
